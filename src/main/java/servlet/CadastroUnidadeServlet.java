2aa3a70c03d7e55d66ebb3e1751bec9e844351d0
mablds <marcelobragalemos@gmail.com>
2020-05-16 14:07:43 -0300

    CadastroUnidadeServlet created

---
+++ src/main/java/servlet/CadastroUnidadeServlet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import dao.UnidadesDAO;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Unidade;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class CadastroUnidadeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        Unidade unidade = new Unidade("Grajau", "Sao Paulo", "Sao Paulo");
+        UnidadesDAO.salvar(unidade);
+    }
+}
