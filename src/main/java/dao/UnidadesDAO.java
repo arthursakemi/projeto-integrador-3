a8b5a95af0f9236398d45f218ba480f9e5b4f3d1
mablds <marcelobragalemos@gmail.com>
2020-05-16 11:55:39 -0300

    Created UnidadesDAO

--- src/main/java/dao/UnidadesDAO.java
+++ src/main/java/dao/UnidadesDAO.java
@@ -5,10 +5,53 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import model.Produto;
+import utils.GerenciadorConexao;
+
 /**
  *
  * @author Marcelo
  */
 public class UnidadesDAO {
-    
+    public static boolean salvar(Produto produto) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO produtos "
+                    + "(nome, categoria, valor, descricao, ativo) "
+                    + "VALUES(?, ?, ?, ?, ?);",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            instrucaoSQL.setString(1, produto.getNome());
+            instrucaoSQL.setString(2, produto.getCategoria());
+            instrucaoSQL.setDouble(3, produto.getValor());
+            instrucaoSQL.setString(4, produto.getDescricao());
+            instrucaoSQL.setBoolean(5, produto.isAtivo());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            retorno = linhasAfetadas > 0;
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            retorno = false;
+        } finally {
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+                GerenciadorConexao.fecharConexao();
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
+    }
 }
