06afbe7179be686e441664b12dc6fc6622d25600
mablds <marcelobragalemos@gmail.com>
2020-05-16 11:35:21 -0300

    update package to utils

--- src/main/java/utils/teste.java
+++ src/main/java/utils/teste.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
+package utils;
 
+import utils.GerenciadorConexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
