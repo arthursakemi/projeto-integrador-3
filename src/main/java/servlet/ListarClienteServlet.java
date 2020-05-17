dbbf3d8fa94499885e6cc087b5f374abc95e0283
mablds <marcelobragalemos@gmail.com>
2020-05-17 13:19:42 -0300

    ListarClienteServlet added to routes and complete

---
+++ src/main/java/servlet/ListarClienteServlet.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import dao.ClienteDAO;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Cliente;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class ListarClienteServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        List<Cliente> clientes = ClienteDAO.listarClientes();
+        request.setAttribute("clientes", clientes);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/clientes.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
