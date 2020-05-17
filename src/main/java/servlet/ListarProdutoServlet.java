f4765d4a7a93cd73dc0246f7046646d52044d8ac
mablds <marcelobragalemos@gmail.com>
2020-05-17 13:04:56 -0300

    ListarProdutoServlet created

--- src/main/java/servlet/ListarProdutoServlet.java
+++ src/main/java/servlet/ListarProdutoServlet.java
@@ -5,10 +5,44 @@
  */
 package servlet;
 
+import dao.ProdutoDAO;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Produto;
+
 /**
  *
  * @author Marcelo
  */
-public class ListarProdutoServlet {
+public class ListarProdutoServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        List<Produto> produtos = ProdutoDAO.listarProdutos();
+        request.setAttribute("produtos", produtos);
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/produtos.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
     
 }
