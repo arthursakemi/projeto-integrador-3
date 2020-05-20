629a37f2e377395c7a7bfdfb9e869798d4bb8ba7
mablds <marcelobragalemos@gmail.com>
2020-05-20 18:57:09 -0300

    ProdutoVenda added

---
+++ src/main/java/model/ProdutoVenda.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class ProdutoVenda {
+    private int idProduto;
+    private int quantidade;
+    
+    public ProdutoVenda(int idProduto, int quantidade) {
+        this.idProduto = idProduto;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+   
+}
